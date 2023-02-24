0034e4f8d4664ed664bc856a3416618ae8c16eac
Threatintel-be <bert@telkom.be>
2023-02-24 14:14:16 +0000

    Updated and added colors

--- email-checker-ti.py
+++ email-checker-ti.py
@@ -1,47 +1,68 @@
 import requests
 import json
+import csv
 import time
+from termcolor import colored
 
-# Load list of emails from emails.txt
+# API endpoint and headers
+api_url = 'https://haveibeenpwned.com/api/v3/breachedaccount/'
+api_headers = {
+    'User-Agent': 'Python script',
+    'hibp-api-key': 'YOUR_API_KEY_HERE'
+}
+
+# Read email addresses from file
 with open('emails.txt', 'r') as f:
-    emails = [line.strip() for line in f]
+    emails = f.read().splitlines()
 
-# Set API key
-api_key = 'your-api-key'
-
-
-# Send request to Have I Been Pwned API for each email
-breaches = {}
+# Process each email address
+breaches = []
 for email in emails:
-    url = f'https://haveibeenpwned.com/api/v3/breachedaccount/{email}'
-    headers = {
-        'User-Agent': 'My Python script',
-        'hibp-api-key': api_key
-    }
-    params = {
-        'truncateResponse': 'false'
-    }
-    response = requests.get(url, headers=headers, params=params, verify=True)
+    # Make API request
+    response = requests.get(api_url + email, headers=api_headers, params={'truncateResponse': 'false'}, verify=True)
 
-    # If the email has been breached, add breach data to dictionary and set pwned flag to True
+    # Handle API response
     if response.status_code == 200:
-        data = response.json()
-        for breach in data:
-            name = breach['Name']
-            date = breach.get('BreachDate', None) or breach.get('AddedDate', None)
-            modified_date = breach.get('ModifiedDate', None)
-            description = breach.get('Description', None)
-            if email not in breaches:
-                breaches[email] = {'pwned': True, 'breaches': []}
-            breaches[email]['breaches'].append({'name': name, 'date': date, 'modified_date': modified_date, 'description': description})
-    # If the email has not been breached, set pwned flag to False
+        # Email address has been breached
+        for breach in response.json():
+            breaches.append({
+                'email': email,
+                'name': breach['Name'],
+                'domain': breach['Domain'],
+                'date': breach['BreachDate'],
+                'added_date': breach['AddedDate'],
+                'modified_date': breach['ModifiedDate'],
+                'description': breach['Description']
+            })
+            print(colored(f'{email} has been breached in {breach["Name"]} on {breach["BreachDate"]}', 'red'))
     elif response.status_code == 404:
-        if email not in breaches:
-            breaches[email] = {'pwned': False, 'breaches': []}
+        # Email address has not been breached
+        breaches.append({
+            'email': email,
+            'name': 'N/A',
+            'domain': 'N/A',
+            'date': 'N/A',
+            'added_date': 'N/A',
+            'modified_date': 'N/A',
+            'description': 'N/A'
+        })
+        print(colored(f'{email} has not been breached', 'green'))
+    else:
+        # Something went wrong with the API request
+        print(colored(f'Error: {response.status_code}', 'yellow'))
 
-    # Wait for 2 seconds before making the next request
-    time.sleep(2)
+    # Sleep for 7 seconds to avoid rate limiting
+    time.sleep(7)
 
-# Dump breaches data to JSON file
+# Write breaches to JSON file
 with open('breaches.json', 'w') as f:
-    json.dump(breaches, f, indent=4)
+    json.dump({'breaches': breaches}, f)
+
+# Write breaches to CSV file
+with open('breaches.csv', 'w', newline='', encoding='utf-8') as f:
+    fieldnames = ['email', 'name', 'domain', 'date', 'added_date', 'modified_date', 'description']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    for breach in breaches:
+        writer.writerow(breach)
+
